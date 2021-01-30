9fc3fcd8d8d1b6a24a948e5fdea6458f1b99b009
Savva Kirichenko <savvakn@gmail.com>
2021-01-30 13:22:46 +0300

    refactor a little bit XD

--- js/main.js
+++ js/main.js
@@ -2,23 +2,43 @@ const generateRandomInt = (min, max) => (
   Math.floor(Math.random() * (max - min + 1) + min)
 );
 
-const getRandomInt = (begin, end) => {
-  if ((begin < 0) || (end < 0)) {
-    throw 'Negative values aren\'t allowed!';
+const generateRandomFloat = (min, max, nDigits) => (
+  (Math.random() * (max - min) + min).toPrecision(nDigits + 1)
+);
+
+const checkErrors = (begin, end) => {
+  if (begin < 0) {
+    throw RangeError('Negative values aren\'t allowed! Use 0 and greater for the Begin.');
+  }
+
+  if (end < 0) {
+    throw RangeError('Negative values aren\'t allowed! Use 0 and greater for the End.');
   }
 
   if (begin === end) {
-    throw 'The Begin and End both have same value!';
+    throw Error('The Begin and End both have same value! They must be different.');
   }
 
   if (begin > end) {
-    throw 'The Begin value exceeds the End value!';
+    throw Error ('The Begin value exceeds the End value! The Begin value must be less the End value.');
   }
+}
+
+const checkNDigit = (nDigit) => {
+  if (nDigit < 0) {
+    throw RangeError('Negative values aren\'t allowed! Use 0 and greater for the nDigit.');
+  }
+}
+
+const getRandomInt = (begin, end) => { // eslint-disable-line
+  checkErrors(begin, end);
 
   return generateRandomInt(begin, end);
 };
 
-const getRandomFloat = (begin, end, decimal) => { // eslint-disable-line
-  const multiplier = 10 ** decimal;
-  return getRandomInt(begin * multiplier, end * multiplier) / multiplier;
+const getRandomFloat = (begin, end, nDigit) => { // eslint-disable-line
+  checkErrors(begin, end);
+  checkNDigit(nDigit);
+
+  return generateRandomFloat(begin, end, nDigit);
 };
