9957b11b41449d484dff4faa1a91e18b69a7e0db
Savva Kirichenko <savvakn@gmail.com>
2021-01-31 23:55:16 +0300

    add functions' description + fixes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
+/**
+ * Get a random integer in range inclusively from 'min' and 'max'.
+ *
+ * @param {number} min - min number
+ * @param {number} max - max number
+ * @return {number} a random integer
+ */
+
 const generateRandomInt = (min, max) => (
   Math.floor(Math.random() * (max - min + 1) + min)
 );
+
+/**
+ * Get a random floating point number in range inclusively from 'min' and 'max', specifying the required number of decimal places with 'nDigits'.
+ *
+ * @param {number} min - min positive number
+ * @param {number} max - max positive number
+ * @param {number} nDigits - nDigits positive number
+ * @return {number} a random floating point number
+ */
 
 const generateRandomFloat = (min, max, nDigits) => (
   (Math.random() * (max - min) + min).toPrecision(nDigits + 1)
@@ -8,27 +25,35 @@ const generateRandomFloat = (min, max, nDigits) => (
 
 const checkErrors = (begin, end) => {
   if (begin < 0) {
-    throw RangeError('Negative values aren\'t allowed! Use 0 and greater for the Begin.');
+    throw new RangeError('Negative values aren\'t allowed! Use 0 and greater for the Begin.');
   }
 
   if (end < 0) {
-    throw RangeError('Negative values aren\'t allowed! Use 0 and greater for the End.');
+    throw new RangeError('Negative values aren\'t allowed! Use 0 and greater for the End.');
   }
 
   if (begin === end) {
-    throw Error('The Begin and End both have same value! They must be different.');
+    throw new RangeError('The Begin and End both have same value! They must be different.');
   }
 
   if (begin > end) {
-    throw Error('The Begin value exceeds the End value! The Begin value must be less the End value.');
+    throw new RangeError('The Begin value exceeds the End value! The Begin value must be less the End value.');
   }
 };
 
 const checkNDigits = (nDigits) => {
   if (nDigits < 0) {
-    throw RangeError('Negative values aren\'t allowed! Use 0 and greater for the nDigits.');
+    throw new RangeError('Negative values aren\'t allowed! Use 0 and greater for the nDigits.');
   }
 };
+
+/**
+ * Get a random positive integer in range inclusively from 'begin' and 'end'.
+ *
+ * @param {number} begin - begin positive number
+ * @param {number} end - end positive number
+ * @return {number} a random positive integer
+ */
 
 const getRandomInt = (begin, end) => { // eslint-disable-line
   checkErrors(begin, end);
@@ -36,7 +61,16 @@ const getRandomInt = (begin, end) => { // eslint-disable-line
   return generateRandomInt(begin, end);
 };
 
-const getRandomFloat = (begin, end, nDigits) => { // eslint-disable-line
+/**
+ * Get a random positive floating point number in range inclusively from 'begin' and 'end', specifying the required number of decimal places with 'nDigits' (default value: 2).
+ *
+ * @param {number} begin - begin positive number
+ * @param {number} end - end positive number
+ * @param {number} nDigits - nDigits positive number
+ * @return {number} a random positive floating point number
+ */
+
+const getRandomFloat = (begin, end, nDigits= 2) => { // eslint-disable-line
   checkErrors(begin, end);
   checkNDigits(nDigits);
 
