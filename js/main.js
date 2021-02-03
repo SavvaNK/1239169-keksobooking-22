d2523eb20e46fca97724eb1a120095510e513e1c
Savva Kirichenko <savvakn@gmail.com>
2021-02-03 19:40:28 +0300

    add location draft

--- js/main.js
+++ js/main.js
@@ -175,8 +175,18 @@ const generateOffer = () => ({
   photos: getUniqRandomItemsFromArray(photosList, getRandomInt(1, photosList.length)),
 })
 
-const offerA = generateOffer();
-const offerB = generateOffer();
-const offerC = generateOffer();
+// location, объект — местоположение в виде географических координат. Состоит из двух полей:
 
-console.log(offerA, offerB, offerC); // eslint-disable-line
+const generateLocation = () => ({ // eslint-disable-line
+  //   x, число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000
+  x: getRandomFloat(35.65000, 35.70000, 5),
+
+  //   y, число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000
+  y: getRandomFloat(139.70000, 139.80000, 5),
+});
+//
+
+const locA = generateLocation();
+const locB = generateLocation();
+const locC = generateLocation();
+console.log(locA, locB, locC); // eslint-disable-line
