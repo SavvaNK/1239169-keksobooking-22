50abd721fc7dd2132cfc6060a59039b91a0d93ee
Savva Kirichenko <savvakn@gmail.com>
2021-02-10 17:26:54 +0300

    move generateAd func to separate file

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
-import generateAuthor from './src/generate-author.js';
-import generateOffer from './src/generate-offer.js';
-import generateLocation from './src/generate-location.js';
-
-const generateAd = () => ({
-  author: generateAuthor(),
-  offer: generateOffer(),
-  location: generateLocation(),
-});
+import generateAd from './src/generate-ad.js';
 
 const ads = new Array(10).fill(null).map(generateAd);
 
