6e60ffaee27daaf0decc1db0312494c08e7404ac
Savva Kirichenko <savvakn@gmail.com>
2021-02-10 17:22:30 +0300

    move generateLocation func to separate file

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
-import getRandomFloat from './utils/get-random-float.js';
 import generateAuthor from './src/generate-author.js';
 import generateOffer from './src/generate-offer.js';
-
-// location, объект — местоположение в виде географических координат. Состоит из двух полей:
-const generateLocation = () => ({ // eslint-disable-line
-  //   x, число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000
-  x: getRandomFloat(35.65000, 35.70000, 5),
-
-  //   y, число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000
-  y: getRandomFloat(139.70000, 139.80000, 5),
-});
+import generateLocation from './src/generate-location.js';
 
 const generateAd = () => ({
   author: generateAuthor(),
