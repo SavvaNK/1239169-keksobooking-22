f71ca9236546c0a18c261bc615aa1d75fe035873
Savva Kirichenko <savvakn@gmail.com>
2021-02-14 01:40:53 +0300

    draft 1

--- js/main.js
+++ js/main.js
@@ -6,3 +6,30 @@ console.log(  // eslint-disable-line
   JSON.stringify(ads),
   ads,
 );
+
+const popupTemplate = document.querySelector('#card')
+  .content
+  .querySelector('.popup');
+
+const mapCanvas = document.querySelector('#map-canvas');
+
+const typeDict = {
+  'palace': 'Дворец',
+  'flat': 'Квартира',
+  'house': 'Дом',
+  'bungalow': 'Бунгало',
+}
+
+ads.forEach((ad) => {
+  const popupClone = popupTemplate.cloneNode(true);
+
+  popupClone.querySelector('.popup__avatar').setAttribute('src', ad.author.avatar);
+  popupClone.querySelector('.popup__title').textContent = ad.offer.title;
+  popupClone.querySelector('.popup__text--address').textContent = ad.offer.address;
+  popupClone.querySelector('.popup__text--price').textContent = `${ad.offer.price} ₽/ночь`;
+  popupClone.querySelector('.popup__type').textContent = typeDict[ad.offer.type];
+
+
+  mapCanvas.appendChild(popupClone);
+});
+
