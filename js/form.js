47dc837341e9e77e1e962dc8d9bc0c44578c7102
Savva Kirichenko <savvakn@gmail.com>
2021-02-18 12:55:09 +0300

    add onchange event to input type

--- js/form.js
+++ js/form.js
@@ -4,3 +4,14 @@ const minPriceForType = {
   house: 5000,
   bungalow: 0,
 };
+
+const inputPrice = document.querySelector('#price');
+const inputType = document.querySelector('#type');
+
+const setMinPrice = () => {
+  const minPriceValue = minPriceForType[inputType['value']];
+  inputPrice.min = minPriceValue;
+  inputPrice.placeholder = minPriceValue;
+};
+
+inputType.addEventListener('change', setMinPrice);
