3c8070226bb3c750fc4d3d6692d3c31a2200f468
Savva Kirichenko <savvakn@gmail.com>
2021-02-20 15:51:17 +0300

    make draft 4

--- js/main.js
+++ js/main.js
@@ -8,20 +8,20 @@ const tokioCenter = {
 
 const disableElement = (el) => el.disabled = true;
 const enableElement = (el) => el.disabled = false;
-const setChildrenProp = (parent, selector, fn) => parent.querySelectorAll(selector).forEach(fn);
+const mapChildren = (parent, selector, fn) => parent.querySelectorAll(selector).forEach(fn);
 
 const disableForm = (formSelector, ...selectors) => {
   const form = document.querySelector(`${formSelector}`);
   form.classList.add(`${formSelector}--disabled`);
 
-  selectors.forEach((selector) => setChildrenProp(form, selector, disableElement));
+  selectors.forEach((selector) => mapChildren(form, selector, disableElement));
 };
 
 const enableForm = (formSelector, ...selectors) => {
   const form = document.querySelector(`${formSelector}`);
   form.classList.remove(`${formSelector}--disabled`);
 
-  selectors.forEach((selector) => setChildrenProp(form, selector, enableElement));
+  selectors.forEach((selector) => mapChildren(form, selector, enableElement));
 };
 
 disableForm('.ad-form', 'fieldset');
