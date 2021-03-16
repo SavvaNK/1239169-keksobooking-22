53d7c004989a553651ed1712abfb640e1d5c6372
Savva Kirichenko <savvakn@gmail.com>
2021-03-16 21:37:57 +0300

    check servers data

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 import './form.js';
 import './map.js';
+
+fetch('https://22.javascript.pages.academy/keksobooking/data')
+  .then((response) => response.json())
+  .then((ads) => {
+    console.log(ads);
+  });
