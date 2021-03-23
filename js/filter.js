9f7dfafe331c68d17e09b7d51ee5523d17a9341f
Savva Kirichenko <savvakn@gmail.com>
2021-03-24 00:35:22 +0300

    add madness 3

--- js/filter.js
+++ js/filter.js
@@ -3,7 +3,7 @@ import { generateAd } from './mock-data/generate-ad.js';
 const ads = new Array(50).fill(null).map(generateAd);
 
 const typeFn = (houseType) => (
-  ({ offer: { type } }) => houseType === 'any' ? type : houseType === type
+  ({ offer: { type } }) => houseType === 'any' ? true : houseType === type
 );
 
 const lowPrice = 10000;
@@ -16,11 +16,25 @@ const price = {
   'high': ({ offer: { price } }) => price >= highPrice,
 };
 
+const rooms = {
+  'any': el => el,
+  '1': ({ offer: { rooms } }) => rooms === 1,
+  '2': ({ offer: { rooms } }) => rooms === 2,
+  '3': ({ offer: { rooms } }) => rooms === 3,
+};
 
+const guests = {
+  'any': el => el,
+  '1': ({ offer: { guests } }) => guests === 1,
+  '2': ({ offer: { guests } }) => guests === 2,
+  '0': ({ offer: { guests } }) => guests === 0,
+};
 
 console.log(
-  ads.filter( typeFn('any')).filter(price['any']).length,
+  ads.filter( typeFn('any')).filter(price['any']).filter(rooms['1']).filter(guests['1']),
   // ads.filter( typeFn('any')).length,
+  '--------------------------------\n',
+  ads.filter( typeFn('any')).filter(price['any']).filter(rooms['1']).filter(guests['1']).length,
   '--------------------------------\n',
   ads.length,
 );
