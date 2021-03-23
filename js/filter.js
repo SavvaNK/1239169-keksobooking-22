a6ea4022a0e9d57367345165a0eca52c3285ac31
Savva Kirichenko <savvakn@gmail.com>
2021-03-24 02:30:43 +0300

    add madness 5

--- js/filter.js
+++ js/filter.js
@@ -1,20 +1,10 @@
 import { generateAd } from './mock-data/generate-ad.js';
 
-const ads = new Array(1000).fill(null).map(generateAd);
+const ads = new Array(10).fill(null).map(generateAd);
 
 const typeFn = (houseType) => (
   ({ offer: { type } }) => houseType === 'any' ? true : houseType === type
 );
-
-const lowPrice = 10000;
-const highPrice = 50000;
-
-const price = {
-  'any': el => el,
-  'middle': ({ offer: { price } }) => price >= lowPrice && price <= highPrice,
-  'low': ({ offer: { price } }) => price <= lowPrice,
-  'high': ({ offer: { price } }) => price >= highPrice,
-};
 
 const roomsFn = (houseRooms) => (
   ({ offer: { rooms } }) => houseRooms === 'any' ? true : parseInt(houseRooms) === rooms
@@ -24,18 +14,24 @@ const guestsFn = (houseGuests) => (
   ({ offer: { guests } }) => houseGuests === 'any' ? true : parseInt(houseGuests) === guests
 );
 
-// const guests = {
-//   'any': el => el,
-//   '1': ({ offer: { guests } }) => guests === 1,
-//   '2': ({ offer: { guests } }) => guests === 2,
-//   '0': ({ offer: { guests } }) => guests === 0,
-// };
+const lowPrice = 10000;
+const highPrice = 50000;
+
+const priceList = {
+  'middle': (price) => price >= lowPrice && price <= highPrice,
+  'low': (price) => price <= lowPrice,
+  'high':(price) => price >= highPrice,
+};
+
+const priceFn = (housePrice) => (
+  ({ offer: { price } }) => housePrice === 'any' ? true : priceList[housePrice](price)
+);
 
 console.log(
-  ads.filter( typeFn('any')).filter(price['any']).filter(roomsFn('1')).filter(guestsFn('1')),
+  ads.filter( typeFn('any')).filter(priceFn('middle')).filter(roomsFn('any')).filter(guestsFn('any')),
   // ads.filter( typeFn('any')).length,
   '--------------------------------\n',
-  ads.filter( typeFn('any')).filter(price['any']).filter(roomsFn('1')).filter(guestsFn('1')).length,
+  ads.filter( typeFn('any')).filter(priceFn('middle')).filter(roomsFn('any')).filter(guestsFn('any')).length,
   '--------------------------------\n',
   ads.length,
 );
