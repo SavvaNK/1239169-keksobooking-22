e28a12aeb2fd288bd7906696798899362d7ecd16
Savva Kirichenko <savvakn@gmail.com>
2021-03-27 13:59:03 +0300

    delete utils dir

--- js/utils/index.js
+++
@@ -1,6 +0,0 @@
-export * from './generate-array-of-strings.js';
-export * from './get-random-float.js';
-export * from './get-random-int.js';
-export * from './get-uniq-random-items-from-array.js';
-export * from './make-counter.js';
-export * from './debounce.js';
